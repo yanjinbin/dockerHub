367313ef26ccb100ea4d0ecaf0ff600b66a982ff
hakuna <yanjinbin@live.com>
2023-09-06 13:34:43 +0800

    done

--- service/mysql/init/01init.sql
+++ service/mysql/init/01init.sql
@@ -1,5 +1,6 @@
 -- 创建用户
 CREATE USER 'test_01'@'%' IDENTIFIED BY '123456';
+CREATE USER 'nacos'@'%' IDENTIFIED BY 'nacos';
 
 -- 创建数据库
 CREATE
@@ -12,7 +13,7 @@ CREATE
 DATABASE  `xxl_job` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 授予权限
-grant all PRIVILEGES on nacos.* to test_01@'%';
+grant all PRIVILEGES on nacos.* to nacos@'%';
 grant all PRIVILEGES on dentist_ease_admin.* to test_01@'%';
 grant all PRIVILEGES on dentist_ease_app.* to test_01@'%';
 grant all PRIVILEGES on xxl_job.* to test_01@'%';
