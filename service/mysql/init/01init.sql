a305c6c42316e2bfdb0aaea5573a10fe98b5fb28
hakuna <yanjinbin@live.com>
2023-09-06 11:35:16 +0800

    fix xxl-job

--- service/mysql/init/01init.sql
+++ service/mysql/init/01init.sql
@@ -8,11 +8,15 @@ CREATE
 DATABASE  `dentist_ease_admin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE
 DATABASE  `dentist_ease_app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE
+DATABASE  `xxl_job` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 授予权限
 grant all PRIVILEGES on nacos.* to test_01@'%';
 grant all PRIVILEGES on dentist_ease_admin.* to test_01@'%';
 grant all PRIVILEGES on dentist_ease_app.* to test_01@'%';
+grant all PRIVILEGES on xxl_job.* to test_01@'%';
+
 
 flush
 privileges;
